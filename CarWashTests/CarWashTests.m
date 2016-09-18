b678f7dd589ab102efd1e952f83f88281b87f2a3
payal-patel <payalpatel23.it@gmail.com>
2016-09-18 12:53:58 +0530

    tests

---
+++ CarWashTests/CarWashTests.m
@@ -0,0 +1,39 @@
+//
+//  CarWashTests.m
+//  CarWashTests
+//
+//  Created by Payal Patel on 29/08/16.
+//  Copyright © 2016 Payal Patel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CarWashTests : XCTestCase
+
+@end
+
+@implementation CarWashTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
